158ae4290b1ba92312f71022179b6b864ea4be75
crazyboomegg <crazyboomegg@gmail.com>
2023-03-24 23:19:31 +0800

    defined  UserListViewModel, UserListViewModelType

--- UserListGitHub/SceneDelegate.swift
+++ UserListGitHub/SceneDelegate.swift
@@ -16,37 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationViewController = UINavigationController(rootViewController: UserListViewController())
+        let viewModel = UserListViewModel()
+        let viewController = UserListViewController(viewModel)
+        let navigationViewController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationViewController
         self.window = window
         window.makeKeyAndVisible()
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 
 }
--- UserListGitHub/UserListViewController.swift
+++ UserListGitHub/UserListViewController.swift
@@ -8,14 +8,28 @@
 import UIKit
 
 class UserListViewController: UIViewController {
-
+    let viewModel: UserListViewModelType
+    init(_ viewModel: UserListViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.red
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+protocol UserListViewModelType {
+    
+}
+
+class UserListViewModel: UserListViewModelType {
+    
+}
