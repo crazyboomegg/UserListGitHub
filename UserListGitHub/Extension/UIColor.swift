2fe67495bcbabe539e05ec36c6f274de2de13d10
crazyboomegg <crazyboomegg@gmail.com>
2023-03-25 05:22:59 +0800

    defined extension UIColor add UserTableViewCell UI

---
+++ UserListGitHub/Extension/UIColor.swift
@@ -0,0 +1,30 @@
+//
+//  UIColor.swift
+//  UserListGitHub
+//
+//  Created by 江柏毅 on 2023/3/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+}
+
--- UserListGitHub/TableViewCell/UserTableViewCell.swift
+++ UserListGitHub/TableViewCell/UserTableViewCell.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class UserTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var baseView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 5
+        view.backgroundColor = UIColor.white
+        view.layer.shadowOffset = CGSize(width: 1.5, height: 3)
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowRadius = 5
+        return view
+    }()
+    
+    private var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "")
+        imageView.layer.cornerRadius = 30
+        return imageView
+    }()
+    
+    private var idLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.black
+        label.text = "江博弈"
+        return label
+    }()
+    
+    private var StaffView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.backgroundColor = UIColor(hexString: "2986cc")
+        
+        let label = UILabel.init()
+        label.text = "STAFF"
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.centerY.equalTo(view)
+            make.centerX.equalTo(view)
+        }
+        
+        return view
+    }()
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 3
+        return stackView
+    }()
+}
