4eee3bcb2ca01eb4d9d9577964e5cb9c9270ce83
crazyboomegg <crazyboomegg@gmail.com>
2023-03-25 00:12:47 +0800

    defined get user list data successfully

--- UserListGitHub/UserListViewController.swift
+++ UserListGitHub/UserListViewController.swift
@@ -21,15 +21,58 @@ class UserListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.red
-        // Do any additional setup after loading the view.
+        bind(to: viewModel)
+        viewModel.getUserList()
     }
-
+    private func bind(to viewModel: UserListViewModelType) {
+        viewModel.userList.observe(on: self) { [weak self] _ in self?.updateUsers() }
+    }
+    private func updateUsers() {
+        print("dick", viewModel.userList.value)
+        
+    }
 }
-
+       
 protocol UserListViewModelType {
-    
+    var userList: Observable<[User]> { get }
+    func getUserList()
 }
 
 class UserListViewModel: UserListViewModelType {
+    let userList: Observable<[User]> = Observable([])
+    func getUserList() {
+        let urlString = "https://api.github.com/users?since=20&per_page=100"
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let jsondata = try decoder.decode([User].self, from: data)
+                self.userList.value = jsondata
+            } catch let error {
+                
+            }
+        }.resume()
+    }
+}
+
+struct User: Decodable {
+    let name: String
+    let image: String
+    let admin: Bool
     
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case image = "avatar_url"
+        case admin = "site_admin"
+    }
 }
