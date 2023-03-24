55c68a2cca72520400ce9a3ac6fd5b5621095c8c
crazyboomegg <crazyboomegg@gmail.com>
2023-03-25 05:50:14 +0800

    defined arrange the file
    UserListViewModel put in ViewModel Group

--- UserListGitHub/UserListViewController.swift
+++ UserListGitHub/UserListViewController.swift
@@ -81,38 +81,6 @@ class UserListViewController: UIViewController {
     }()
 }
 
-protocol UserListViewModelType {
-    var userList: Observable<[User]> { get }
-    func getUserList()
-}
-
-class UserListViewModel: UserListViewModelType {
-    let userList: Observable<[User]> = Observable([])
-    func getUserList() {
-        let urlString = "https://api.github.com/users?since=20&per_page=100"
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error)
-                return
-            }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                return
-            }
-            guard let data = data else {
-                return
-            }
-            do {
-                let decoder = JSONDecoder()
-                let jsondata = try decoder.decode([User].self, from: data)
-                self.userList.value = jsondata
-            } catch let error {
-                
-            }
-        }.resume()
-    }
-}
-
 struct User: Decodable {
     let name: String
     let image: String
